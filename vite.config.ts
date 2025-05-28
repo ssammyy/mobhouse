e64e6d53630b594b6590fbea84015b8706c8b7a1
ssammyy <samuikumbu@gmail.com>
2025-05-28 11:02:40 +0300

    Fix: Update Vite config for both development and production environments

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mobhouse/',
+  base: process.env.NODE_ENV === 'production' ? '/mobhouse/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
